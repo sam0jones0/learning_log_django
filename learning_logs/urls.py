b9acfed07963fdd37f06ce1ea8236554ce4dfb90
Sam Jones <sam0jones0@gmail.com>
2020-10-30 13:38:42 +0000

    Added ability to add new topics, entries and to edit existing ones.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Page for user to add new topic.
+    path('topics/new_form/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry to a topic.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page for editing an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
